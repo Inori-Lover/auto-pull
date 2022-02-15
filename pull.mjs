cbb7f9119267d3e097efe68c4c34ccc652fab7ad
inori_lover <inori_lover@outlook.com>
2022-02-16 00:18:56 +0800

    feat: 添加黑名单路径；调试递归

--- pull.mjs
+++ pull.mjs
@@ -6,6 +6,15 @@
 // zx pull.mjs --entry './'
 // zx pull.mjs --entry 'work'
 
+const BAN_LIST = new Set([
+  '.pnpm-store',
+  '.circleci',
+  '.codesandbox',
+  '.github',
+  'node_modules',
+  '$RECYCLE.BIN',
+]);
+
 /**
  * @param {string} folder
  *
@@ -18,6 +27,10 @@ function isGitFolder(folder) {
   }
 
   for (const child of children) {
+    if (BAN_LIST.has(child)) {
+      return false;
+    }
+
     if (child === ".git") {
       return fs.statSync(path.resolve(folder, child)).isDirectory();
     }
@@ -27,7 +40,7 @@ function isGitFolder(folder) {
 }
 
 // 获取入口指定，默认当前目录
-let entry = ".";
+let entry = process.cwd();
 if (argv.entry) {
   entry = argv.entry;
 
@@ -35,20 +48,28 @@ if (argv.entry) {
     entry = path.resolve(".", entry);
   }
 }
+
+// entry = path.format(path.parse(path.normalize(entry)))
+// console.log('entry',entry);
+
 // 枚举当前目录下的所有文件夹
 const childrenName = fs.readdirSync(entry);
 for (const child of childrenName) {
   const childPath = path.resolve(entry, child);
   const stats = fs.statSync(childPath);
   if (!stats.isDirectory()) {
-    break;
+    continue;
   }
 
-  if (isGitFolder(childPath)) {
+  if (BAN_LIST.has(child)) {
+    continue;
+  }
+
+  if (isGitFolder(path.resolve(entry, childPath))) {
+    console.log("");
+    console.log(chalk.green(new Date().toJSON() + ":"), "begin >>>");
+    cd(childPath);
     try {
-      console.log("");
-      console.log(chalk.green(new Date().toJSON() + ":"), "begin >>>");
-      cd(childPath);
       await $`git fetch --prune --prune-tags --all --tags --quiet`;
 
       console.log(chalk.green(new Date().toJSON() + ":"), "done <<<");
@@ -56,8 +77,11 @@ for (const child of childrenName) {
       // 控制台本来就有输出，这里吃掉error就行
       console.log(chalk.red(new Date().toJSON() + ":"), "error <<<");
     }
+    cd(entry);
   } else {
-    // @todo 递归
+    // todo: 处理初始context
+    // console.log('childPath',childPath);
+    // await $`zx pull.mjs --entry ${childPath}`
   }
 }
 
